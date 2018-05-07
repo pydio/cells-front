4130503fa79ab7924cd980558298bd33de536864
Greg Hecquet <greg.tf.hecquet@gmail.com>
2018-05-07 18:05:57 +0200

    Libre office driver

--- plugins/editor.libreoffice/res/js/editor.js
+++ plugins/editor.libreoffice/res/js/editor.js
@@ -41,30 +41,18 @@ class Editor extends React.Component {
     }
 
     componentWillMount() {
-        let iframeUrl = configs.get('LIBREOFFICE_IFRAME_URL'),
-            webSocketSecure = configs.get('LIBREOFFICE_WEBSOCKET_SECURE'),
-            webSocketHost = configs.get('LIBREOFFICE_WEBSOCKET_HOST'),
-            webSocketPort = configs.get('LIBREOFFICE_WEBSOCKET_PORT');        
-        
-        // FIXME: was retrieved from the response JSON before, we manually add the prefix otherwise collabora cannot get the doc. 
-        let host = 'http://'+webSocketHost;
-        // TODO also manage backend port when we have found a solution for the collabora container 
-        // to call the backend on a specific port. For the time being, all request that are sent to:
-        // mypydiohost.example.com/wopi/... must be proxied to the correct host, f.i. mypydiohost.example.com:5014/wopi
-        // via a reverse proxy. 
-
-        let webSocketProtocol = webSocketSecure ? 'wss' : 'ws',
-        webSocketUrl = encodeURIComponent(`${webSocketProtocol}://${webSocketHost}:${webSocketPort}`);
-
         let fileName = this.props.node.getPath();
         pydio.ApiClient.request({
             get_action: 'libreoffice_get_file_url',
             file: fileName
         }, ({responseJSON = {}}) => {
             //was (see above): let {host, uri, permission, jwt} = responseJSON;
-            let {uri, permission, jwt} = responseJSON;
+            let {host, uri, permission, jwt} = responseJSON;
             let fileSrcUrl = encodeURIComponent(`${host}${uri}`);
-            this.setState({url: `${iframeUrl}?host=${webSocketUrl}&WOPISrc=${fileSrcUrl}&access_token=${jwt}&permission=${permission}`});
+            let webSocketUrl = encodeURIComponent(`${host}`);
+            webSocketUrl = webSocketUrl.replace(/^https/, 'wss');
+            webSocketUrl = webSocketUrl.replace(/^http/, 'ws');
+            this.setState({url: `/loleaflet/dist/loleaflet.html?host=${webSocketUrl}&WOPISrc=${fileSrcUrl}&access_token=${jwt}&permission=${permission}`});
         });
     }
 
