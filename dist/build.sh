eba5c38f67118c71816fc35487f0abb06803338e
cdujeu <charles@ajaxplorer.info>
2018-06-12 16:14:08 +0200

    Remove clearcache, not useful

--- dist/build.sh
+++ dist/build.sh
@@ -32,9 +32,6 @@ sed -i "s/##REVISION##/$COMMIT/g" conf/*
 sed -i "s/##VERSION_DATE##/$DATE/g" conf/*
 sed -i "s/##VERSION_NUMBER##/$BUILD_NAME/g" conf/*
 
-echo "Clearing Composer cache"
-composer clearcache
-
 # Compile Sources
 echo "Running Composer in core and plugins"
 find . -maxdepth 3 -name composer.json -execdir composer install \;
